b52b47b8e91e1f29697919ffdfcfc14816eddc83
Boris Muriel <boris.muriel@educatic.com.co>
2024-12-05 19:57:09 -0500

    implementacion idiomas

--- scripts/idiomas.js
+++ scripts/idiomas.js
@@ -13,30 +13,13 @@ document.getElementById('idiomas-tab').addEventListener('click', function () {
 });
 
 function addLanguage() {
-  const addRowBtn = document.getElementById('add-language-column-btn');
-  if (addRowBtn) {
-    addRowBtn.addEventListener('click', function () {
-      const tableBody = document.querySelector("#language-table tbody");
-      
-      // Create a new row
-      const newRow = document.createElement("tr");
+  document.getElementById('btnAdicionarIdioma').addEventListener('click', function() {
+    document.getElementById('form-idioma-row').style.display = 'block';
+    document.getElementById('btnCancelarIdiomas').style.display = 'inline-block';
+  });
 
-      // Add cells to the new row
-      newRow.innerHTML = `
-        <td><input type="text" class="form-control" placeholder="Puntaje"></td>
-        <td><input type="text" class="form-control" placeholder="Nivel"></td>
-        <td><input type="text" class="form-control" placeholder="Nombre de la prueba"></td>
-        <td><input type="text" class="form-control" placeholder="Fecha de presentación"></td>
-        <td>
-          <div class="d-flex flex-row justify-content-around">
-            <i class="bi bi-pencil-square text-primary p-2 pointer"></i>
-            <i class="bi bi-trash text-danger p-2 pointer"></i>
-          </div>
-        </td>
-      `;
-
-      // Append the new row to the table body
-      tableBody.appendChild(newRow);
-    });
-  }
+  document.getElementById('btnCancelarIdiomas').addEventListener('click', function() {
+    document.getElementById('form-idioma-row').style.display = 'none';
+    document.getElementById('btnCancelarIdiomas').style.display = 'none';
+  });
 }
